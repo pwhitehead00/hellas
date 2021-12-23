a24bcb7f5020fcb8d49377f5af93acf701495fde
Paul Whitehead <whitehead.pauljay@gmail.com>
2021-12-22 23:57:45 -0700

    refactor route groups

--- api/v1/module.go
+++ api/v1/module.go
@@ -8,33 +8,38 @@ import (
 )
 
 // https://registry.terraform.io/v1/modules/terraform-aws-modules/vpc/aws/3.11.0/download
-func download(c *gin.Context) {
-	namespace := c.Param("namespace")
-	provider := c.Param("provider")
-	name := c.Param("name")
-	version := c.Param("version")
-	url := fmt.Sprintf("git::https://github.com/%s/terraform-%s-%s?ref=v%s", namespace, provider, name, version)
+func addDownload(rg *gin.RouterGroup) {
+	download := rg.Group("/modules")
 
-	c.Header("X-Terraform-Get", url)
-	c.Status(204)
+	download.GET("/:namespace/:name/:provider/:version/download", func(c *gin.Context) {
+		namespace := c.Param("namespace")
+		provider := c.Param("provider")
+		name := c.Param("name")
+		version := c.Param("version")
+		url := fmt.Sprintf("git::https://github.com/%s/terraform-%s-%s?ref=v%s", namespace, provider, name, version)
+
+		c.Header("X-Terraform-Get", url)
+		c.Status(204)
+	})
 }
 
 // https://github.com/terraform-aws-modules/terraform-aws-vpc
 // https://registry.terraform.io/v1/modules/terraform-aws-modules/vpc/aws/versions
-func version(c *gin.Context) {
-	namespace := c.Param("namespace")
-	provider := c.Param("provider")
-	name := c.Param("name")
+func addVersion(rg *gin.RouterGroup) {
+	versions := rg.Group("/modules")
 
-	o := gh.MegaGitHub(namespace, provider, name)
+	versions.GET("/:namespace/:name/:provider/versions", func(c *gin.Context) {
+		namespace := c.Param("namespace")
+		provider := c.Param("provider")
+		name := c.Param("name")
 
-	c.JSON(200, o)
+		o := gh.MegaGitHub(namespace, provider, name)
+		c.JSON(200, o)
+	})
 }
 
-func ModuleRegistry(r *gin.Engine) {
-	v1 := r.Group("/v1/modules")
-	{
-		v1.GET("/:namespace/:name/:provider/:version/download", download)
-		v1.GET("/:namespace/:name/:provider/versions", version)
-	}
+func ModuleRegistry(rg *gin.Engine) {
+	v1 := rg.Group("/v1")
+	addVersion(v1)
+	addDownload(v1)
 }
