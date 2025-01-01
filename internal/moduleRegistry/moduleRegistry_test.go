6bc86812a53f2598b1a9bdb826bb95fc177117e9
Paul Whitehead <whitehead.pauljay@gmail.com>
2025-01-01 14:20:24 -0700

    add tests for discovery and health check handlers

--- internal/moduleRegistry/moduleRegistry_test.go
+++ internal/moduleRegistry/moduleRegistry_test.go
@@ -1,6 +1,9 @@
 package moduleregistry
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,6 +33,68 @@ func TestNewModuleRegistry(t *testing.T) {
 		t.Run(tC.desc, func(t *testing.T) {
 			_, err := NewModuleRegistry(tC.input)
 			assert.Equal(t, tC.err, err)
+		})
+	}
+}
+
+func TestDiscoveryEndpoint(t *testing.T) {
+	testCases := []struct {
+		desc       string
+		statusCode int
+		body       string
+	}{
+		{
+			desc:       "successful response",
+			statusCode: http.StatusOK,
+			body:       "{\"modules.v1\":\"/v1/modules/\"}\n",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/.well-known/terraform.json", nil)
+			w := httptest.NewRecorder()
+			discovery(w, req)
+
+			resp := w.Result()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				assert.Fail(t, err.Error(), "unexpected error reading response body")
+			}
+			defer resp.Body.Close()
+
+			assert.Equal(t, tC.body, string(body))
+			assert.Equal(t, tC.statusCode, resp.StatusCode)
+		})
+	}
+}
+
+func TestHealthCheck(t *testing.T) {
+	testCases := []struct {
+		desc       string
+		statusCode int
+		body       string
+	}{
+		{
+			desc:       "successful response",
+			statusCode: http.StatusOK,
+			body:       "\"ok\"\n",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/healthcheck", nil)
+			w := httptest.NewRecorder()
+			healthCheck(w, req)
+
+			resp := w.Result()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				assert.Fail(t, err.Error(), "unexpected error reading response body")
+			}
+			defer resp.Body.Close()
+
+			assert.Equal(t, tC.body, string(body))
+			assert.Equal(t, tC.statusCode, resp.StatusCode)
 		})
 	}
 }
